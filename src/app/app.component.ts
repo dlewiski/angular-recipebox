c5288ffa529443c98056033169e9b33203f3caeb
David Lewis and Stephanie Faber <david@>
2018-03-26 11:10:19 -0700

    set up recipe box using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  recipes: Recipe[] = [
+    new Recipe("Tomato Soup", ["salt", "pepper", "tomatos"], "boil tomatoes in water for 15 minutes, add salt and pepper to taste, pour that shit in a bowel and sip it bc your a badass and you don't fuck with spoons"),
+    new Recipe("Potato Soup", ["salt", "pepper", "potatoes"], "boil potatoes in water for 15 minutes, add salt and pepper to taste, pour that shit in a bowel and sip it bc your a badass and you don't fuck with spoons"),
+    new Recipe("Clam Chowder", ["salt", "pepper", "clams"], "simmer clams in heavy cream for 15 minutes you don't need to do anything else according to Stef. and She knows. So don't mess. ")
+  ];
 }
