5cb646ff2453d5bc115fc95d5eccf0eaef6946ed
David Lewis and Stephanie Faber <david@>
2018-03-26 16:39:28 -0700

    finalize project for the day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from './models/recipe.model';
 export class AppComponent {
   recipes: Recipe[] = [
     new Recipe("Tomato Soup", ["salt", "pepper", "tomatos"], "boil tomatoes in water for 15 minutes, add salt and pepper to taste, pour that shit in a bowel and sip it bc your a badass and you don't fuck with spoons"),
-    new Recipe("Potato Soup", ["salt", "pepper", "potatoes"], "boil potatoes in water for 15 minutes, add salt and pepper to taste, pour that shit in a bowel and sip it bc your a badass and you don't fuck with spoons"),
-    new Recipe("Clam Chowder", ["salt", "pepper", "clams"], "simmer clams in heavy cream for 15 minutes you don't need to do anything else according to Stef. and She knows. So don't mess. ")
+    new Recipe("Potato Soup", ["salt", "pepper", "potatoes"], "boil potatoes in water for 15 minutes, add salt and pepper to taste, pour that shit in a bowl and sip it bc your a badass and you don't fuck with spoons"),
+    new Recipe("Clam Chowder", ["salt", "pepper", "clams", "heavy cream"], "simmer clams in heavy cream for 15 minutes you don't need to do anything else according to Stef. and She knows. So don't mess. ")
   ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
